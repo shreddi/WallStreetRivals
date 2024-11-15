e4e98500f88e5ecd69a3a74e39e5f8ac2954b6bf
Jason Swartz <jasonrswartz@gmail.com>
2024-11-14 17:49:41 -0700

    dockerized frontend and backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  // esbuild: {
+  //   jsxInject: `import React from 'react'`,
+  // },
+  // optimizeDeps: {
+  //   force: true
+  // },
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  //   setupFiles: './vitest.setup.ts',
+  // },
 })
