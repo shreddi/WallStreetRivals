fcabf25c645f2a87ef39222c37b30fee87a57fd3
Jason Swartz <jasonrswartz@gmail.com>
2024-11-21 12:11:13 -0700

    website icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,52 +1,29 @@
-import { useState, useEffect } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'\
-import axios from 'axios';
+import React from 'react'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
 import './App.css'
+import Portfolio from './components/Portfolio';
 
-// function App() {
-//   const [count, setCount] = useState(0)
-  
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
 
 function App() {
-  const [message, setMessage] = useState('');
 
-  useEffect(() => {
-      axios.get('http://localhost:8000/api/hello/')
-          .then(response => {
-            setMessage(response.data.message)
-          })
-          .catch(error => {
-            console.error('Error fetching data:', error)
-          })
-  }, []);
-
-  return <div>{message}</div>;
+  return (
+    <React.StrictMode>
+      <MantineProvider
+        theme={{
+          fontFamily: 'Montserrat, sans-serif',
+          headings: { fontFamily: 'Montserrat, sans-serif' },
+        }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate replace to="/portfolio" />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+          </Routes>
+        </BrowserRouter>
+      </MantineProvider>
+    </React.StrictMode>
+  );
 }
 
 export default App
---
+++ frontend/src/components/Portfolio.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, Flex, Box, Text, TextInput, ActionIcon, Pagination,} from '@mantine/core';
+
+const Portfolio: React.FC = () => {
+    return(
+        <Text>
+            hello
+        </Text>
+    )
+}
+
+export default Portfolio
