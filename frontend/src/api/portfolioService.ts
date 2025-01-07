7f88ef0e71f7a19888e23a72f64782feee10970f
Jason Swartz <jasonrswartz@gmail.com>
2025-01-07 13:59:14 -0700

    Portfolio APIs now include access token in header.

--- frontend/src/api/portfolioService.ts
+++ frontend/src/api/portfolioService.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import { Stock, Portfolio, Holding } from '../types.ts'
-import { API_BASE_URL, axiosConfig, buildUrl } from './apiService.ts';
+import { API_BASE_URL, buildUrl, getAuthHeaders } from './apiService.ts';
 
 //Portfolio API Service
 export const portfolioApi = {
     async getPortfolio(portfolio_id: number): Promise<Portfolio> {
+        const headers = getAuthHeaders()
+
         console.log(API_BASE_URL)
         try {
-            const response = await axios.get(buildUrl(`/api/portfolios/${portfolio_id}/`), axiosConfig);
+            const response = await axios.get(buildUrl(`/api/portfolios/${portfolio_id}/`), {headers, withCredentials: true});
             return response.data;
         } catch (error) {
             console.error(`Failed to get portfolio ${portfolio_id}:`, error);
@@ -16,8 +18,10 @@ export const portfolioApi = {
     },
 
     async createPortfolio(portfolio: Partial<Portfolio>): Promise<Portfolio> {
+        const headers = getAuthHeaders()
+
         try {
-            const response = await axios.post(buildUrl('/api/portfolios/'), portfolio, axiosConfig);
+            const response = await axios.post(buildUrl('/api/portfolios/'), portfolio, {headers, withCredentials: true});
             return response.data;
         } catch (error) {
             console.error('Failed to create portfolio:', error);
@@ -26,11 +30,13 @@ export const portfolioApi = {
     },
 
     async updatePortfolio(portfolio: Portfolio): Promise<Portfolio> {
+        const headers = getAuthHeaders()
+
         if (!portfolio.id) {
             throw new Error('Portfolio ID is required to update a portfolio.');
         }
         try {
-            const response = await axios.put(buildUrl(`/api/portfolios/${portfolio.id}/`), portfolio, axiosConfig);
+            const response = await axios.put(buildUrl(`/api/portfolios/${portfolio.id}/`), portfolio, {headers, withCredentials: true});
             return response.data;
         } catch (error) {
             console.error(`Failed to update portfolio ${portfolio.id}:`, error);
@@ -39,8 +45,10 @@ export const portfolioApi = {
     },
 
     async deletePortfolio(portfolio_id: number): Promise<void> {
+        const headers = getAuthHeaders()
+
         try {
-            await axios.delete(buildUrl(`/api/portfolios/${portfolio_id}/`), axiosConfig);
+            await axios.delete(buildUrl(`/api/portfolios/${portfolio_id}/`), {headers, withCredentials: true});
         } catch (error) {
             console.error(`Failed to delete portfolio ${portfolio_id}:`, error);
             throw error;
@@ -52,8 +60,10 @@ export const portfolioApi = {
 export const stockApi = {
 
     async getAllStocks(): Promise<Stock[]> {
+        const headers = getAuthHeaders()
+
         try {
-            const response = await axios.get(buildUrl('/api/stocks'), axiosConfig);
+            const response = await axios.get(buildUrl('/api/stocks'), {headers, withCredentials: true});
             return response.data;
         } catch (error) {
             console.error('Failed to get all stocks:', error);
@@ -68,8 +78,10 @@ export const stockApi = {
 export const holdingApi = {
 
     async createHolding(holding: Holding): Promise<Holding> {
+        const headers = getAuthHeaders()
+
         try {
-            const response = await axios.post(buildUrl('/api/holdings/'), holding, axiosConfig);
+            const response = await axios.post(buildUrl('/api/holdings/'), holding, {headers, withCredentials: true});
             console.log(response.data)
             return response.data
         } catch (error) {
@@ -79,11 +91,13 @@ export const holdingApi = {
     },
 
     async updateHolding(holding: Holding): Promise<Holding> {
+        const headers = getAuthHeaders()
+
         if (!holding.id) {
             throw new Error('Holding ID is required to update a holding.');
         }
         try {
-            const response = await axios.put(buildUrl(`/api/holdings/${holding.id}/`), holding, axiosConfig);
+            const response = await axios.put(buildUrl(`/api/holdings/${holding.id}/`), holding, {headers, withCredentials: true});
             return response.data;
         } catch (error) {
             console.error(`Failed to update holding ${holding.id}:`, error);
@@ -92,8 +106,10 @@ export const holdingApi = {
     },
 
     async deleteHolding(holding_id: number): Promise<void> {
+        const headers = getAuthHeaders()
+
         try {
-            await axios.delete(buildUrl(`/api/holdings/${holding_id}/`), axiosConfig);
+            await axios.delete(buildUrl(`/api/holdings/${holding_id}/`), {headers, withCredentials: true});
         } catch (error) {
             console.error(`Failed to delete holding ${holding_id}:`, error);
             throw error;
