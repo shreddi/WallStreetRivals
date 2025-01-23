17d188c4a8caba9c754734b261d1da165585c23e
Jason Swartz <jasonrswartz@gmail.com>
2025-01-23 04:12:48 -0700

    add logo

--- frontend/src/components/appShell/AppShell.tsx
+++ frontend/src/components/appShell/AppShell.tsx
@@ -6,10 +6,14 @@ import {
     Button,
     Container,
     Center,
+    Group,
+    Image,
+    Box,
 } from "@mantine/core";
 import { useAccount } from "../../contexts/useAccount";
 import UserDropdown from "./UserDropdown";
 import { useNavigate } from "react-router-dom";
+import Logo from "../../assets/WSR-logo-black-vert.jpg";
 
 // This component provides the structure of the app including the header.
 
@@ -24,48 +28,73 @@ export default function BasicAppShell({
     return (
         <AppShell header={{ height: 140 }}>
             <AppShell.Header bg="black" p="md">
-                <Stack>
-                    <Flex align="center" justify="Flex-end" gap="md">
-                        <Title order={6} c="white" fw={700} tt="uppercase">
-                            welcome, {currentAccount?.username}
-                        </Title>
-                        <UserDropdown />
-                    </Flex>
-                    <Flex align="center" justify="Flex-end" gap="md">
-                        <Button
-                            onClick={() => navigate("/my_contests")}
-                            variant="transparent"
-                        >
+                <Group justify="space-between" align="flex-start" h={140}>
+                    <Box h={140} w={300}>
+                        <Image src={Logo} h={100} fit="contain" />
+                    </Box>
+                    <Stack justify="center">
+                        <Flex align="center" justify="Flex-end" gap="md">
                             <Title order={6} c="white" fw={700} tt="uppercase">
-                                my contests
+                                welcome, {currentAccount?.username}
                             </Title>
-                        </Button>
-                        <Button
-                            onClick={() => navigate("/open_contests")}
-                            variant="transparent"
-                        >
-                            <Title order={6} c="white" fw={700} tt="uppercase">
-                                open contests
-                            </Title>
-                        </Button>
-                        <Button
-                            onClick={() => navigate("/dashboard")}
-                            variant="transparent"
-                        >
-                            <Title order={6} c="white" fw={700} tt="uppercase">
-                                dashboard
-                            </Title>
-                        </Button>
-                        <Button
-                            onClick={() => navigate("/new_league")}
-                            variant="transparent"
-                        >
-                            <Title order={6} c="white" fw={700} tt="uppercase">
-                                new league (+)
-                            </Title>
-                        </Button>
-                    </Flex>
-                </Stack>
+                            <UserDropdown />
+                        </Flex>
+                        <Flex align="center" justify="Flex-end" gap="md">
+                            <Button
+                                onClick={() => navigate("/my_contests")}
+                                variant="transparent"
+                            >
+                                <Title
+                                    order={6}
+                                    c="white"
+                                    fw={700}
+                                    tt="uppercase"
+                                >
+                                    my contests
+                                </Title>
+                            </Button>
+                            <Button
+                                onClick={() => navigate("/open_contests")}
+                                variant="transparent"
+                            >
+                                <Title
+                                    order={6}
+                                    c="white"
+                                    fw={700}
+                                    tt="uppercase"
+                                >
+                                    open contests
+                                </Title>
+                            </Button>
+                            <Button
+                                onClick={() => navigate("/dashboard")}
+                                variant="transparent"
+                            >
+                                <Title
+                                    order={6}
+                                    c="white"
+                                    fw={700}
+                                    tt="uppercase"
+                                >
+                                    dashboard
+                                </Title>
+                            </Button>
+                            <Button
+                                onClick={() => navigate("/new_league")}
+                                variant="transparent"
+                            >
+                                <Title
+                                    order={6}
+                                    c="white"
+                                    fw={700}
+                                    tt="uppercase"
+                                >
+                                    new league (+)
+                                </Title>
+                            </Button>
+                        </Flex>
+                    </Stack>
+                </Group>
             </AppShell.Header>
 
             <AppShell.Main>
