eab42af60f6f4dc0367f6b9c9249a27fe29f72ad
Jason Swartz <jasonrswartz@gmail.com>
2025-01-22 18:01:00 -0700

    fix HTML for email.

--- backend/app/services/email_service.py
+++ backend/app/services/email_service.py
@@ -2,9 +2,10 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 
+
 def send_invitation_email(player, contest):
-    subject = f"You're Invited to Join {contest.name}"
-    message = render_to_string(
+    subject = f"You're Invited to Join '{contest.name}'"
+    html_message = render_to_string(
         "contest_invite_email.html",
         {
             "player": player,
@@ -13,9 +14,10 @@ def send_invitation_email(player, contest):
         },
     )
     send_mail(
-        subject,
-        message,
-        settings.DEFAULT_FROM_EMAIL,
-        [player.email],
+        subject=subject,
+        message="You're invited!",
+        html_message=html_message,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[player.email],
         fail_silently=False,
     )
